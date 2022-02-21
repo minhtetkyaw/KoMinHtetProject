a5be36b92405ef04bc8bffe0f10bc114b50aebe3
Aung Ko Ko Khant <aungkokokhant16@gmail.com>
2022-02-21 15:57:03 +0630

    first commit

--- js/main.js
+++ js/main.js
@@ -23,16 +23,7 @@ $(document).ready(function () {
         $nav.toggleClass('collapse');
     })
 
-    // owl-crousel for blog
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        autoplay: false,
-        autoplayTimeout: 3000,
-        dots: false,
-        nav: true,
-        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
-        responsive: responsive
-    });
+    
 
 
     // click to scroll top
@@ -46,3 +37,42 @@ $(document).ready(function () {
     AOS.init();
 
 });
+
+$('.responsive').slick({
+    dots: false,
+    arrows: true,
+    rtl: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+              
